d871eaade7a183e31b46159f128e2ea0168481d2
KitYan20 <kyan@bu.edu>
2024-10-30 17:53:20 -0400

    updated description

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -12,9 +12,10 @@ export default function HeroSection() {
                         Aspiring SWE
                     </h1>
                     <p className="hero--section-description">
-                        I am currently a senior majoring in Linguistics and Computer Science at BU.
-                        <br/>
-                        I'm looking for Full Time new Graduate Roles.
+                        I am a highly driven Boston University Computer Science and Linguistics graduate with strong technical skills seeking a full-time 
+position as a Junior Software Development and IT Support Specialist. Eager to contribute to a forward-thinking organization 
+by solving complex problems, work on maintaining high quality code, and ensuring the seamless operation of IT systems. 
+                        
                         <br/>
                         <br/>
 
